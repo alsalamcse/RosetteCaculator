e432df32f151f92d91b92014f37557949675adc9
alsalamcc <alsalamcc17>
2017-10-11 12:25:59 +0300

    rosette

--- app/src/main/java/com/draz/rosette/rosettecaculator/CalculatorActivity.java
+++ app/src/main/java/com/draz/rosette/rosettecaculator/CalculatorActivity.java
@@ -7,24 +7,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener{
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnZa2d, btnNa2s, btnDrb, btn2smi, btnKowa, btnYsawi, btnClear;
-    private TextView tvResult, etNum1, etNum2;
+    private TextView tvResult, etNum1, etNum2, tvQues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
-        btnZa2d=(Button)findViewById(R.id.btnZa2d);
-        btnNa2s=(Button)findViewById(R.id.btnNa2s);
-        btnDrb=(Button)findViewById(R.id.btnDrb);
-        btn2smi=(Button)findViewById(R.id.btn2smi);
-        btnKowa=(Button)findViewById(R.id.btnKowa);
-        btnYsawi=(Button)findViewById(R.id.btnYsawi);
-        btnClear=(Button)findViewById(R.id.btnClear);
-        tvResult=(TextView)findViewById(R.id.tvResult);
-        etNum1=(EditText)findViewById(R.id.etNum1);
-        etNum2=(EditText)findViewById(R.id.etNum2);
+        btnZa2d = (Button) findViewById(R.id.btnZa2d);
+        btnNa2s = (Button) findViewById(R.id.btnNa2s);
+        btnDrb = (Button) findViewById(R.id.btnDrb);
+        btn2smi = (Button) findViewById(R.id.btn2smi);
+        btnKowa = (Button) findViewById(R.id.btnKowa);
+        btnYsawi = (Button) findViewById(R.id.btnYsawi);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        tvQues=(TextView)findViewById(R.id.tvQues);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+        etNum1 = (EditText) findViewById(R.id.etNum1);
+        etNum2 = (EditText) findViewById(R.id.etNum2);
         btnZa2d.setOnClickListener(this);
         btnNa2s.setOnClickListener(this);
         btnDrb.setOnClickListener(this);
@@ -33,24 +34,64 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
         btnYsawi.setOnClickListener(this);
         btnClear.setOnClickListener(this);
 
-      
 
     }
 
     @Override
     public void onClick(View view) {
-        if (view==btnZa2d)
-        {
-            String stNum1=etNum1.getText().toString();
-            double num1=Double.parseDouble(stNum1);
 
-            String stNum2=etNum2.getText().toString();
-            double num2=Double.parseDouble(stNum2);
 
-           // tvResult.setText(num1*num2);
+        if (view == btnZa2d) {
+            tvQues.setText("+");
+        }
+        if (view == btnNa2s) {
+            tvQues.setText("-");
+        }
+        if (view == btnDrb) {
+            tvQues.setText("x");
+        }
+        if (view == btn2smi) {
+            tvQues.setText("/");
+        }
+
+        if (view == btnKowa) {
+            tvQues.setText("^");
+        }
+
+        if (view == btnYsawi) {
+            String stNum1 = etNum1.getText().toString();
+            double num1 = Double.parseDouble(stNum1);
+
+            String stNum2 = etNum2.getText().toString();
+            double num2 = Double.parseDouble(stNum2);
+            String stQues =tvQues.getText().toString();
+            if (stQues.equals("+"))
+            {
+                tvResult .setText((num1+num2)+"");
+            }
+            if (stQues.equals("-"))
+            {
+                tvResult .setText((num1-num2)+"");
+            }
+            if (stQues.equals("/"))
+            {
+                tvResult .setText((num1/num2)+"");
+            }
+            if (stQues.equals("x"))
+            {
+                tvResult .setText((num1*num2)+"");
+            }
+            if (stQues.equals("^"))
+            {
+                tvResult.setText((Math.pow(num1,num2))+"");
+            }
+        }
+        if (view == btnClear) {
+            tvResult.setText(" ");
+            etNum1.setText((" "));
+            etNum2.setText(" ");
+        }
+        }
 
         }
 
-
-    }
-}
